063d233c785b5d6a30f769a28491fd4b7b9290cf
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2024-01-04 21:14:48 +0900

    fix : userentity 수정

--- src/main/java/com/sparta/library/entity/User.java
+++ src/main/java/com/sparta/library/entity/User.java
@@ -10,9 +10,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "u_id",nullable = false)
-    private String u_id;
+    @Column(name = "name",nullable = false)
+    private String name;
 
-    @Column(name = "password",nullable = false)
-    private String password;
+    @Column(name = "gender",nullable = false)
+    private String gender;
+
+    @Column(name = "idNumber",nullable = false)
+    private String idNumber;
+
+    @Column(name = "phoneNumber",nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address",nullable = false)
+    private String address;
+
 }
